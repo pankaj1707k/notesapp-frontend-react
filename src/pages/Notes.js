e092e7e7c0c38530ba8c658042d67c84948e3557
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-07 19:50:56 +0530

    Offcanvas menu for category list on screen sizes below medium breakpoint

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -5,14 +5,31 @@ const Notes = () => {
   return (
     <main className="container">
       <div className="row mt-4">
-        <div className="col-4 px-4">
-          <h4 className="d-flex justify-content-between">
-            <span>Categories</span>
-            <i class="bi bi-plus-lg cursor-pointer"></i>
-          </h4>
-          <CategoryList />
+        <button
+          className="btn btn-outline-primary col-1 ms-3 w-auto d-md-none"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#categorylist"
+          aria-controls="categorylist"
+        >
+          <i class="bi bi-arrow-bar-right"></i>
+        </button>
+        <div
+          className="offcanvas-md offcanvas-start col-md-4 px-4 py-2 border border-primary border-top-0 border-bottom-0 border-start-0"
+          tabIndex="-1"
+          id="categorylist"
+        >
+          <div className="offcanvas-body">
+            <div className="w-100">
+              <h4 className="d-flex justify-content-between">
+                <span>Categories</span>
+                <i class="bi bi-plus-lg cursor-pointer"></i>
+              </h4>
+              <CategoryList />
+            </div>
+          </div>
         </div>
-        <div className="col-8">Notes list section</div>
+        <div className="col-11 col-md-8 px-3 py-2">Notes list section</div>
       </div>
     </main>
   );
