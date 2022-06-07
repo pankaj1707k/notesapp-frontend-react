8347a3f158b49e3baa0882e0fea7b12e9458e552
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-07 17:45:00 +0530

    Reduced letter spacing for home phrase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <main className="container">
-      <h1 className="display-4 mt-5">
+      <h1 className="display-4 mt-5 home-phrase">
         <strong>Create and access your sticky notes anywhere</strong>
       </h1>
       <Link to="/" className="btn btn-lg btn-primary mt-3">
