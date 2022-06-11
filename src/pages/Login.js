c73f6bc50931f8a990886978d9235d1cd0ceb8b8
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-11 22:09:07 +0530

    States for signup and login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import FormInputGroup from "../components/FormInputGroup";
 
 const Login = () => {
+  var [username, setUsername] = useState("");
+  var [password, setPassword] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ username, password });
+    // API call to register user
+    setUsername("");
+    setPassword("");
+  };
+
   return (
     <main className="container d-flex flex-column align-items-center">
       <h1 className="h1 mt-5">Login to your account</h1>
-      <form className="mt-3 w-75">
-        <FormInputGroup icon="bi bi-at fs-3" type="text" name="Username" />
-        <FormInputGroup icon="bi bi-key fs-3" type="password" name="Password" />
+      <form className="mt-3 w-75" onSubmit={handleSubmit}>
+        <FormInputGroup
+          icon="bi bi-at fs-3"
+          type="text"
+          name="Username"
+          value={username}
+          onChange={handleUsername}
+        />
+        <FormInputGroup
+          icon="bi bi-key fs-3"
+          type="password"
+          name="Password"
+          value={password}
+          onChange={handlePassword}
+        />
         <button type="submit" className="btn btn-primary btn-lg w-100">
           Login
         </button>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import FormInputGroup from "../components/FormInputGroup";
 
 const Signup = () => {
+  var [username, setUsername] = useState("");
+  var [email, setEmail] = useState("");
+  var [password, setPassword] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ username, email, password });
+    // API call to register user
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <main className="container d-flex flex-column align-items-center">
       <h1 className="h1 mt-5">New User</h1>
-      <form className="mt-3 w-75">
-        <FormInputGroup icon="bi bi-at fs-3" type="text" name="Username" />
-        <FormInputGroup icon="bi bi-envelope fs-3" type="email" name="Email" />
-        <FormInputGroup icon="bi bi-key fs-3" type="password" name="Password" />
+      <form className="mt-3 w-75" onSubmit={handleSubmit}>
+        <FormInputGroup
+          icon="bi bi-at fs-3"
+          type="text"
+          name="Username"
+          value={username}
+          onChange={handleUsername}
+        />
+        <FormInputGroup
+          icon="bi bi-envelope fs-3"
+          type="email"
+          name="Email"
+          value={email}
+          onChange={handleEmail}
+        />
+        <FormInputGroup
+          icon="bi bi-key fs-3"
+          type="password"
+          name="Password"
+          value={password}
+          onChange={handlePassword}
+        />
         <button type="submit" className="btn btn-primary btn-lg w-100">
           Register
         </button>
